05af335f8c5dcedb4fe66d97a254209fb046772e
MarqCervMartin <martin_mcervantes@hotmail.com>
2022-04-03 01:06:57 -0600

    crate project

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React, {FC} from 'react';
+
+export const App: FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
